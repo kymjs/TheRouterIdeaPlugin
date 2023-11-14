d76f24e5e4985affa5039617ba92557799fd1fe2
张涛(tao281.zhang) <tao281.zhang@huolala.cn>
2023-11-14 13:17:26 +0800

    优化读取速度

--- src/main/kotlin/cn/therouter/idea/navigator/TheRouterLineMarker.kt
+++ src/main/kotlin/cn/therouter/idea/navigator/TheRouterLineMarker.kt
@@ -6,7 +6,6 @@ import com.intellij.codeInsight.navigation.NavigationGutterIconBuilder
 import com.intellij.openapi.editor.markup.GutterIconRenderer
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiFile
 import com.intellij.psi.PsiManager
 import com.intellij.psi.search.FilenameIndex
 import com.intellij.psi.search.GlobalSearchScope
@@ -63,6 +62,7 @@ class TheRouterLineMarker : LineMarkerProvider {
             }
         }
 
+        val allMarkerStatus = HashMap<String, Int>()
         elements.forEach { element ->
             val key = getKey(filePath, element)
             allMarkerPsi[key]?.let { pair ->
@@ -71,35 +71,41 @@ class TheRouterLineMarker : LineMarkerProvider {
 
                 val all = allTargetPsi[key] ?: HashSet()
                 if (all.isNotEmpty()) {
-                    val builder = NavigationGutterIconBuilder.create(getIcon(targetContent.type))
-                    builder.setAlignment(GutterIconRenderer.Alignment.CENTER)
-                    builder.setTargets(all)
-                    if (targetContent.type == TYPE_ROUTE_ANNOTATION || targetContent.type == TYPE_ACTION_INTERCEPT) {
-                        builder.setTooltipTitle("TheRouter:跳转到使用处")
-                    } else {
-                        builder.setTooltipTitle("TheRouter:跳转到声明处")
-                    }
-                    result.add(builder.createLineMarkerInfo(psiElement))
-                } else {
-                    val builder = NavigationGutterIconBuilder.create(getIcon(TYPE_NONE))
-                        .setAlignment(GutterIconRenderer.Alignment.CENTER)
-                        .setTargets(listOf())
-
-                    if (targetContent.type == TYPE_ROUTE_ANNOTATION || targetContent.type == TYPE_ACTION_INTERCEPT) {
-                        builder.setTooltipTitle("未发现使用:TheRouter.build(${targetContent.code})")
-                    } else {
-                        val path = targetContent.code
-                        if (!path.contains('"') && !path.isFirstUpper()) {
-                            builder.setTooltipTitle("变量 ${targetContent.code} 内容未知，请开发者检查是否定义路由")
+                    if (allMarkerStatus[key] != STATUS_SHOWN) {
+                        allMarkerStatus[key] = STATUS_SHOWN
+                        val builder = NavigationGutterIconBuilder.create(getIcon(targetContent.type))
+                        builder.setAlignment(GutterIconRenderer.Alignment.CENTER)
+                        builder.setTargets(all)
+                        if (targetContent.type == TYPE_ROUTE_ANNOTATION || targetContent.type == TYPE_ACTION_INTERCEPT) {
+                            builder.setTooltipTitle("TheRouter:跳转到使用处")
                         } else {
-                            if (psiElement.text.contains("action(")) {
-                                builder.setTooltipTitle("未定义 ActionInterceptor(${targetContent.code})")
+                            builder.setTooltipTitle("TheRouter:跳转到声明处")
+                        }
+                        result.add(builder.createLineMarkerInfo(psiElement))
+                    }
+                } else {
+                    if (!allMarkerStatus.containsKey(key)) {
+                        allMarkerStatus[key] = STATUS_ERROR
+                        val builder = NavigationGutterIconBuilder.create(getIcon(TYPE_NONE))
+                            .setAlignment(GutterIconRenderer.Alignment.CENTER)
+                            .setTargets(listOf())
+
+                        if (targetContent.type == TYPE_ROUTE_ANNOTATION || targetContent.type == TYPE_ACTION_INTERCEPT) {
+                            builder.setTooltipTitle("未发现使用:TheRouter.build(${targetContent.code})")
+                        } else {
+                            val path = targetContent.code
+                            if (!path.contains('"') && !path.isFirstUpper()) {
+                                builder.setTooltipTitle("变量 ${targetContent.code} 内容未知，请开发者检查是否定义路由")
                             } else {
-                                builder.setTooltipTitle("未声明 @Route(path=${targetContent.code})")
+                                if (psiElement.text.contains("action(")) {
+                                    builder.setTooltipTitle("未定义 ActionInterceptor(${targetContent.code})")
+                                } else {
+                                    builder.setTooltipTitle("未声明 @Route(path=${targetContent.code})")
+                                }
                             }
                         }
+                        result.add(builder.createLineMarkerInfo(psiElement))
                     }
-                    result.add(builder.createLineMarkerInfo(psiElement))
                 }
             }
         }
@@ -131,28 +137,31 @@ class TheRouterLineMarker : LineMarkerProvider {
     ): Collection<TargetPsiElement> {
         val result = HashSet<TargetPsiElement>()
 
-        if (fileSystemCache.isEmpty()) {
+        val runnable = Runnable {
             val scopes = GlobalSearchScope.projectScope(project)
             val kotlinFiles = FilenameIndex.getAllFilesByExt(project, "kt", scopes)
             val javaFiles = FilenameIndex.getAllFilesByExt(project, "java", scopes)
             val allCodeFiles = ArrayList(kotlinFiles)
             allCodeFiles.addAll(javaFiles)
-            if (allCodeFiles.isNullOrEmpty()) {
-                return HashSet()
-            }
             for (virtualFile in allCodeFiles) {
                 if (virtualFile.canonicalPath == filePath) {
                     continue
                 }
-                val psiFile: PsiFile? = PsiManager.getInstance(project).findFile(virtualFile)
-                psiFile ?: return HashSet()
-
-                val properties = PsiTreeUtil.findChildrenOfType(psiFile, PsiElement::class.java)
-                properties.forEach {
-                    fileSystemCache.add(Cache(it, psiFile.name))
+                val psiFile = PsiManager.getInstance(project).findFile(virtualFile)
+                psiFile?.let {
+                    val properties = PsiTreeUtil.findChildrenOfType(psiFile, PsiElement::class.java)
+                    properties.forEach {
+                        fileSystemCache.add(Cache(it, psiFile.name))
+                    }
                 }
             }
         }
+
+        runnable.run()
+//        if (fileSystemCache.isEmpty()) {
+//        } else {
+//            Thread(runnable).start()
+//        }
 
         fileSystemCache.forEach { cache ->
             // 根据当前content类型，查找需要跳转的目标代码
