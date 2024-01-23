b70a51b91c0fba2c78fff05b9d6c47a45792a388
张涛(tao281.zhang) <tao281.zhang@huolala.cn>
2024-01-23 11:08:41 +0800

    update

--- src/main/kotlin/cn/therouter/idea/Manifest.kt
+++ src/main/kotlin/cn/therouter/idea/Manifest.kt
@@ -1,26 +1,26 @@
 package cn.therouter.idea
 
-import cn.therouter.idea.navigator.handlePath
+//import cn.therouter.idea.navigator.handlePath
 
-const val PLUGIN_VERSION_NAME = "1.2.3"
-const val PLUGIN_VERSION_CODE = 123
-const val LATEST_LIBRARY_VERSION_NAME = "1.2.0-rc5"
-const val RELEASE_LIBRARY_VERSION_NAME = "1.1.4"
+const val PLUGIN_VERSION_NAME = "1.2.6"
+const val PLUGIN_VERSION_CODE = 126
+const val LATEST_LIBRARY_VERSION_NAME = "1.2.2-rc5"
+const val RELEASE_LIBRARY_VERSION_NAME = "1.2.1"
 
 
 fun main() {
-    val content = "@Route(path=HOME)"
-    val path = "HOME"
-
-    val result = content.contains(Regex("path=(\\S*\\.)*${handlePath(path)},")) || content.contains(
-        Regex(
-            "path=(\\S*\\.)*${
-                handlePath(
-                    path
-                )
-            }\\)"
-        )
-    )
-
-    println(result)
+//    val content = "@Route(path=HOME)"
+//    val path = "HOME"
+//
+//    val result = content.contains(Regex("path=(\\S*\\.)*${handlePath(path)},")) || content.contains(
+//        Regex(
+//            "path=(\\S*\\.)*${
+//                handlePath(
+//                    path
+//                )
+//            }\\)"
+//        )
+//    )
+//
+//    println(result)
 }
--- src/main/kotlin/cn/therouter/idea/update/UpgradeComponent.kt
+++ src/main/kotlin/cn/therouter/idea/update/UpgradeComponent.kt
@@ -2,28 +2,26 @@ package cn.therouter.idea.update
 
 import cn.therouter.idea.PLUGIN_VERSION_CODE
 import cn.therouter.idea.utils.getVersion
-import com.intellij.notification.NotificationDisplayType
-import com.intellij.notification.NotificationGroup
-import com.intellij.notification.NotificationType
+import cn.therouter.idea.utils.gotoUrl
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.project.ProjectManagerListener
+import com.intellij.openapi.startup.StartupActivity
+import com.intellij.openapi.ui.MessageDialogBuilder
+import com.intellij.openapi.ui.Messages
 
-class UpgradeComponent : ProjectManagerListener {
-    override fun projectOpened(project: Project) {
-        super.projectOpened(project)
+class UpgradeComponent : StartupActivity {
+
+    override fun runActivity(project: Project) {
         val version = getVersion()
 
         if (version.toolVersionCode > PLUGIN_VERSION_CODE) {
-            val notificationGroup = NotificationGroup(
-                displayId = "TheRouter",
-                displayType = NotificationDisplayType.BALLOON
-            )
-
-            notificationGroup.createNotification(
-                title = "TheRouter：有新的 AS 插件可更新",
-                content = "请在 AndroidStudio 插件市场更新至" + version.toolVersionName,
-                type = NotificationType.INFORMATION
-            ).notify(project)
+            if (MessageDialogBuilder.okCancel("有新的 AS 插件可更新", "请在 AndroidStudio 插件市场更新至" + version.toolVersionName)
+                            .noText("关闭")
+                            .yesText("查看")
+                            .icon(Messages.getInformationIcon())
+                            .ask(project)
+            ) {
+                gotoUrl("https://plugins.jetbrains.com/plugin/20047-therouter/")
+            }
         }
     }
 }
