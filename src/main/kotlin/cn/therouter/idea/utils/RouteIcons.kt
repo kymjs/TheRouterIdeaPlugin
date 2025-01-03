caf3e2c3344134bf0f48d5474ee69b73f8ec3486
zhangtao <kymjs@zhangtaodeMacBook-Pro-2.local>
2025-01-03 09:50:56 +0800

    修改icon路径

--- src/main/kotlin/cn/therouter/idea/utils/RouteIcons.kt
+++ src/main/kotlin/cn/therouter/idea/utils/RouteIcons.kt
@@ -5,9 +5,9 @@ import com.intellij.openapi.util.IconLoader.getIcon
 
 object RouteIcons {
     @JvmField
-    val icon_from = getIcon("/icon/icon_from.png", RouteIcons::class.java)
+    val icon_from = getIcon("/icons/icon_from.png", RouteIcons::class.java)
     @JvmField
-    val icon_to = getIcon("/icon/icon_to.png", RouteIcons::class.java)
+    val icon_to = getIcon("/icons/icon_to.png", RouteIcons::class.java)
     @JvmField
-    val icon_warn = getIcon("/icon/icon_warn.png", RouteIcons::class.java)
+    val icon_warn = getIcon("/icons/icon_warn.png", RouteIcons::class.java)
 }
