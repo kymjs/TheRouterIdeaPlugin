0c8f78a4438c820b3ea8f7375717369d18020902
张涛(tao281.zhang) <tao281.zhang@huolala.cn>
2025-04-11 18:35:59 +0800

    修改版本号

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "cn.therouter"
-version = "1.3.2"
+version = "1.3.3"
 
 repositories {
     maven("https://maven.therouter.cn:8443/repository/maven-public/")
--- src/main/kotlin/cn/therouter/idea/Manifest.kt
+++ src/main/kotlin/cn/therouter/idea/Manifest.kt
@@ -3,8 +3,8 @@ package cn.therouter.idea
 import com.intellij.openapi.project.Project
 import java.io.File
 
-const val PLUGIN_VERSION_NAME = "1.3.2"
-const val PLUGIN_VERSION_CODE = 132
+const val PLUGIN_VERSION_NAME = "1.3.3"
+const val PLUGIN_VERSION_CODE = 133
 const val LATEST_LIBRARY_VERSION_NAME = "1.2.4-rc13"
 const val RELEASE_LIBRARY_VERSION_NAME = "1.2.3"
 const val LATEST_HARMONY_VERSION_NAME = "1.0.0-rc7"
